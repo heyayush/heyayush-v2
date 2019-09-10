db37fbfe39e497f56e2cfb51cff360c3b7f07757
Ayush <ayush15may@gmail.com>
2019-09-10 19:10:43 +0530

    adding thank-you page

--- src/components/contact-form/contact-form.js
+++ src/components/contact-form/contact-form.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './contact-form.scss';
 
 const contactForm = () => (
-  <form name="contact" className="form" id="contact-form" method="POST" data-netlify="true">
+  <form name="contact" className="form" id="contact-form" action="/thank-you" method="POST" data-netlify="true">
     <input type="hidden" name="form-name" value="contact" />
     <div className="form-title">Or, you may fill this form to reach me</div>
     <p className="name">
