7ddb1ec06061bc3ea731cf255acedc16e2dd6f52
Ayush <ayush15may@gmail.com>
2019-09-10 16:11:02 +0530

    Adding netlify contact form

---
+++ src/components/contact-form/contact-form.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './contact-form.scss';
+
+const contactForm = () => (
+  <form className="form" id="contact-form" method="POST" data-netlify="true">
+    <div className="form-title">Or, you may fill this form to reach me</div>
+    <p className="name">
+      <input name="name" type="text" className="feedback-input" placeholder="Name" id="name" />
+    </p>
+
+    <p className="email">
+      <input name="email" type="text" className="feedback-input" id="email" placeholder="Email" />
+    </p>
+
+    <p className="message">
+      <textarea name="message" className="feedback-input" id="comment" placeholder="Comment"></textarea>
+    </p>
+
+    <div className="submit">
+      <button type="submit" id="button-blue">
+        SEND
+      </button>
+    </div>
+  </form>
+);
+
+export default contactForm;
